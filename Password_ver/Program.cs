c6fb73faeed0a2c8b9ceb88ad02aa8765a6f3208
PriyaBasavaraj <priya.basavaraj.03@gmail.com>
2017-01-16 17:45:26 +0530

    Password validation

---
+++ Password_ver/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_ver
+{
+    public class PasswordValidator
+    {
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public bool Verify(string Password, object usertype)
+        {
+            if (usertype.GetType()==typeof(InternalPwdValidator))
+            {
+                InternalPwdValidator ipv = (InternalPwdValidator)usertype;
+                return ipv.IntVerifyPwd(Password);
+            }
+            else
+            {
+                ExternalPwdValidator ipv = (ExternalPwdValidator)usertype;
+                return ipv.ExtVerifyPwd(Password);
+            }          
+        }
+    }
+
+    public class ExternalPwdValidator : PasswordValidator
+    {
+        public bool ExtVerifyPwd(string Password)
+        {
+            int condition = 0;
+            bool check = true;
+
+            //check for null
+            if (string.IsNullOrEmpty(Password))
+                throw new Exception("Password cannot be Null.");
+            else
+                condition++;
+
+            //check for minimum length
+            if (Password.Length < 8)
+                throw new Exception("Password length should be greater than 8 characters");
+            else
+                condition++;
+
+            //check for upper case letter
+            if ((Password.Any(x => char.IsUpper(x))))
+                condition++;
+            else
+            {
+                check = false;
+                throw new Exception("Password should have one uppercase character.");
+
+            }
+
+            //check for lower case letter
+            if ((Password.Any(x => char.IsLower(x))))
+                condition++;
+            else
+                throw new Exception("Password should have one lowercase character.");
+
+            //check for number
+            if ((Password.Any(x => char.IsNumber(x))))
+                condition++;
+            else
+                throw new Exception("Password should have one number character.");
+
+            //to return value
+            if (condition >= 3 && check == true)
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class InternalPwdValidator : PasswordValidator
+    {
+        public bool IntVerifyPwd(string Password)
+        {
+            bool check = true;
+            //check for minimum length
+            if (Password.Length <8 )
+            { 
+                check = false;
+                throw new Exception("Password length should be greater than 8 characters");
+            }
+            return check;
+        }
+     }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ pwd_ver_test/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Password_ver;
+
+namespace pwd_ver_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        PasswordValidator pv = new PasswordValidator();
+        [TestMethod]
+        public void GivingCorrectPassword_doesNotThrowAnException()
+        {
+            ExternalPwdValidator e = new ExternalPwdValidator();
+            bool result = pv.Verify("abcD123xyz",e);
+            Assert.AreEqual(true, result, "error");
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void GivingEmptyPassword_VerifyingIt_ShouldThrowExceptionMessage()
+        {
+            ExternalPwdValidator e = new ExternalPwdValidator();
+            bool result = pv.Verify("",e);
+            Assert.AreEqual(false, result, "error");
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void GivingPasswordLessThan8Characters_VerifyingIt_ShouldThrowExceptionMessage()
+        {
+            ExternalPwdValidator e = new ExternalPwdValidator();
+            bool result = pv.Verify("ag3C",e);
+            Assert.AreEqual(false, result, "error");
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void GivingAllSmallCharactersAsPassword_VerifyingIt_ShouldThrowExceptionMessage()
+        {
+            ExternalPwdValidator e = new ExternalPwdValidator();
+            bool result = pv.Verify("abcbcdefghi",e);
+            Assert.AreEqual(false, result, "error");
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void GivingNoSmallCharactersInAPassword_VerifyingIt_ShouldThrowExceptionMessage()
+        {
+            ExternalPwdValidator e = new ExternalPwdValidator();
+            bool result = pv.Verify("ABCD12345",e);
+            Assert.AreEqual(false, result, "error");
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void GivingNoNumbersInAPassword_VerifyingIt_ShouldThrowExceptionMessage()
+        {
+            ExternalPwdValidator e = new ExternalPwdValidator();
+            bool result = pv.Verify("ABCDcvhsfk",e);
+            Assert.AreEqual(false, result, "error");
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void PasswordNotSatisfyingThreeConditions_VerifyingIt_ShouldThrowExceptionMessage()
+        {
+            ExternalPwdValidator e = new ExternalPwdValidator();
+            bool result = pv.Verify("ab",e);
+            Assert.AreEqual(false, result, "error");
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void GivingNoBigCharactersInAPassword_VerifyingIt_ShouldThrowExceptionMessage()
+        {
+            ExternalPwdValidator e = new ExternalPwdValidator();
+            bool result = pv.Verify("ab",e);
+            Assert.AreEqual(false, result, "error");
+        }
+
+        [TestMethod]
+        public void GivingCharactersLengthGreaterThan8InAPassword_VerifyingIt_DoesThrowExceptionMessage()
+        {
+            InternalPwdValidator i = new InternalPwdValidator();
+            bool result = pv.Verify("jadvoiadv76", i);
+            Assert.AreEqual(true, result, "error");
+        }
+
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void GivingCharactersLengthLessThan8InAPassword_VerifyingIt_DoesThrowExceptionMessage()
+        {
+            InternalPwdValidator i = new InternalPwdValidator();
+            bool result = pv.Verify("abc", i);
+            Assert.AreEqual(false, result, "error");
+        }
+    }
+}
